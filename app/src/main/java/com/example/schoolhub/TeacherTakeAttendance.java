6f2ebbe2c396ea16aaf42e0978ef5e116fcbd6ec
abdar7eem <abdar7eem04@gmail.com>
2025-05-11 02:08:52 +0300

    Abd 2:07

---
+++ app/src/main/java/com/example/schoolhub/TeacherTakeAttendance.java
@@ -0,0 +1,21 @@
+package com.example.schoolhub;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class TeacherTakeAttendance extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_teacher_take_attendance, container, false);
+    }
+}
