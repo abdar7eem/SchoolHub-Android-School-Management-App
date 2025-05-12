cf21a8480a4f1e8395ddf453e0f1be03904696e1
Anas Al Sayed <167682013+AnasAlSayed18@users.noreply.github.com>
2025-05-13 00:04:43 +0300

    Fixing StudentScheduleFragment

--- app/src/main/java/com/example/schoolhub/Student/Adapter/ScheduleAdapter.java
+++ app/src/main/java/com/example/schoolhub/Student/Adapter/ScheduleAdapter.java
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.schoolhub.Model.Schedule;
+import com.example.schoolhub.R;
 
 import java.util.List;
 
@@ -40,7 +41,7 @@ public class ScheduleAdapter extends BaseAdapter {
         Schedule schedule = scheduleList.get(position);
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.schedule_card, parent, false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item_student_schedule, parent, false);
         }
 
         TextView subject = convertView.findViewById(R.id.txtSubjectName);
