d4209044c23f40072c182fa264df93a946a3f532
Anas Al Sayed <167682013+AnasAlSayed18@users.noreply.github.com>
2025-05-13 04:52:16 +0300

    Run The StudentAssignmentsFragment 1.1

--- app/src/main/java/com/example/schoolhub/Model/Assignment.java
+++ app/src/main/java/com/example/schoolhub/Model/Assignment.java
@@ -1,4 +1,22 @@
 package com.example.schoolhub.Model;
-
 public class Assignment {
+    private String title;
+    private String subjectName;
+    private String teacherName;
+    private String dueDate;
+    private String status;
+
+    public Assignment(String title, String subjectName, String teacherName, String dueDate, String status) {
+        this.title = title;
+        this.subjectName = subjectName;
+        this.teacherName = teacherName;
+        this.dueDate = dueDate;
+        this.status = status;
+    }
+
+    public String getTitle() { return title; }
+    public String getSubjectName() { return subjectName; }
+    public String getTeacherName() { return teacherName; }
+    public String getDueDate() { return dueDate; }
+    public String getStatus() { return status; }
 }
