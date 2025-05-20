b31694bb72da5d857a8d8f9dd3b4a88e0833e067
AhmadToffaha <ahmadmoatadil26@gmail.com>
2025-05-21 02:39:54 +0300

    EveryThing Done BAAAAAAAAABBBBYYYYYYYYYYYYYY

--- app/src/main/java/com/example/schoolhub/MainActivity.java
+++ app/src/main/java/com/example/schoolhub/MainActivity.java
@@ -2,23 +2,10 @@ package com.example.schoolhub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.schoolhub.Student.StudentMainActivity;
-import com.example.schoolhub.Teacher.TeacherMainActivity;
-import com.google.android.material.navigation.NavigationView;
-
-import org.json.JSONException;
+import com.example.schoolhub.Registrar.RegistrarMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, StudentMainActivity.class));
+        startActivity(new Intent(this, RegistrarMainActivity.class));
         finish(); // Prevent back navigation to this splash
     }
 
