8e6e58169ac07f305d8cd6794694d7dffc722c35
Anas Al Sayed <167682013+AnasAlSayed18@users.noreply.github.com>
2025-06-09 16:43:48 +0300

    Do the last updated 6/8/6:02...

--- app/src/main/java/com/example/schoolhub/Registration/LoginActivity.java
+++ app/src/main/java/com/example/schoolhub/Registration/LoginActivity.java
@@ -33,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPrefs;
 
-    public static final     String baseUrl = "http://192.168.56.1/SchoolHub/";
+    public static final     String baseUrl = "http://192.168.88.6/SchoolHub/";
 
 
 
